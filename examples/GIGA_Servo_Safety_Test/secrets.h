723b6f5de59117d3a7eab8de0f347f560e28b6d6
Alexandros Charitonidis <58593787+Alexandros-Charitonidis@users.noreply.github.com>
2026-05-16 13:41:40 +0100

    Delete examples/GIGA_Servo_Safety_Test/secrets.h

--- examples/GIGA_Servo_Safety_Test/secrets.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-// Copy this file to secrets.h and update the values for your classroom network.
-
-#define WIFI_SSID "PhaseSpaceNetwork_2.4G"
-#define WIFI_PASSWORD "8igMacNet"
-#define BROKER_HOST "192.168.0.74"
-#define BROKER_PORT 1883
-#define GROUP_ID "5"
